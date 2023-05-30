97a3b5739c3a8950076f04ad6a4d427b75961373
Shazil Sattar <sattarshazil@gmail.com>
2023-05-30 11:21:46 +0500

    fix: added build in test ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   setupFiles: ['<rootDir>/src/tests/setup/test-env-setup.ts'],
   setupFilesAfterEnv: ['<rootDir>/src/tests/setup/test-setup.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/src/tests', '/src/scripts', '/build/', '/coverage/'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/', '/coverage/'],
   testEnvironment: 'node',
 }
