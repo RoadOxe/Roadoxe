e90ca535e638b4386730f53c323437e13f0a0598
usman azeem <iamusmanazeem@gmail.com>
2023-06-12 17:22:23 +0500

    feat: wrote test cases for aws resources and refacotred some code.

--- infra/cdk/test/cdk.test.ts
+++ infra/cdk/test/cdk.test.ts
@@ -1,17 +1,43 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as Cdk from "../lib/main-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("MainStack", () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeAll(() => {
+    app = new cdk.App();
+    stack = new Cdk.MainStack(app, "MainStack", {
+      env: {
+        account: "746789327319",
+        region: "eu-north-1",
+      },
+    });
+    template = Template.fromStack(stack);
+  });
+
+  it("S3 bucket should be Created", () => {
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      BucketName: "roadoxe-s3",
+    });
+  });
+
+  it("Security Group for EC2 should be Created", () => {
+    template.hasResourceProperties("AWS::EC2::SecurityGroup", {
+      GroupName: "roadoxe-sg",
+    });
+  });
+
+  it("EC2 instance should be Created", () => {
+    template.hasResourceProperties("AWS::EC2::Instance", {
+      Tags: [
+        {
+          Key: "Name",
+          Value: "roadoxe-ec2-instance",
+        },
+      ],
+    });
+  });
 });
